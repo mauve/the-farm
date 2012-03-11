691b5de176cee2acc4a0630a5b51464348b7a9be
Mikael Olenfalk <olenfalk@spotify.com>
2012-03-11 18:29:48 +0100

    Added util::type_traits::remove_cv_ref

---
+++ src/util/type_traits/remove_cv_ref.hpp
@@ -0,0 +1,24 @@
+
+#ifndef UTIL_TYPE_TRAITS_REMOVE_CV_REF_HPP_
+#define UTIL_TYPE_TRAITS_REMOVE_CV_REF_HPP_
+
+#include <boost/type_traits/remove_cv.hpp>
+#include <boost/type_traits/remove_reference.hpp>
+
+namespace util {
+
+namespace type_traits {
+
+template <typename T>
+struct remove_cv_ref
+{
+	typedef typename boost::remove_reference <
+				typename boost::remove_cv< T >::type
+			>::type type;
+};
+
+}  // namespace type_traits
+
+}  // namespace util
+
+#endif /* UTIL_TYPE_TRAITS_REMOVE_CV_REF_HPP_ */
