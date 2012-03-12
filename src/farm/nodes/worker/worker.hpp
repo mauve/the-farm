30c022f1df0d402d34e5c6b518872962b675bd78
Mikael Olenfalk <olenfalk@spotify.com>
2012-03-12 21:01:30 +0100

    initial non-working worker

---
+++ src/farm/nodes/worker/worker.hpp
@@ -0,0 +1,43 @@
+
+#ifndef FARM_NODES_WORKER_HPP_
+#define FARM_NODES_WORKER_HPP_
+
+#include <boost/asio/io_service.hpp>
+
+#include <network/protocol/dispatcher.hpp>
+#include <network/transport/message_passer.hpp>
+
+namespace farm {
+
+namespace nodes {
+
+namespace worker {
+
+class Worker
+{
+public:
+	Worker (boost::asio::io_service& io_service,
+			network::transport::message_passer::pointer mpi);
+	~Worker ();
+
+	void start ();
+	void stop ();
+
+private:
+	void _on_queue_job (const jobs::JobOrder& order);
+	void _on_query_job_queues ();
+	void _on_cancel_job ();
+
+private:
+	boost::asio::io_service& _io_service;
+	network::transport::message_passer::pointer _mpi;
+	network::protocol::dispatcher<Worker> _dispatcher;
+};
+
+}  // namespace worker
+
+}  // namespace nodes
+
+}  // namespace farm
+
+#endif /* FARM_NODES_WORKER_HPP_ */
---
+++ src/farm/nodes/worker/worker_main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int main (int argc, char* argv[])
+{
+	std::cout << "Hello Worker" << std::endl;
+	return 0;
+}
+
